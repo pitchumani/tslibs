5ec7d903e2b7583e38ed9e920926ca9ecaa0f3b2
Pitchumani Sivanupandi <s.pitchumani@gmail.com>
2025-06-14 11:22:37 -0400

    Added thread-safe queue implementation TSQueue

---
+++ examples/queue-example.cpp
@@ -0,0 +1,51 @@
+// This example uses a standard queue in multithreaded context.
+// It is not thread-safe, expected to fail if run in parallel.
+
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <thread>
+
+class MyClass {
+    int data;
+public:
+    MyClass(int data) : data(data) {}
+};
+
+class Worker {
+public:
+    static void Task(std::shared_ptr<std::queue<MyClass>> myqueue, int thread_id, int iterations) {
+        for (int i = 0; i < iterations; ++i) {
+            myqueue->emplace(thread_id * 1000 + i);
+            for (int j = 0; j < 10000; ++j) { /* do nothing, just simulate delay */ }
+            if (!myqueue->empty()) myqueue->pop();
+        }
+    }
+};
+
+int main() {
+    // queue is assumed to be a thread-safe queue supporting concurrent push and pop operations.
+    // It is used here to safely share data between multiple threads.
+        auto myqueue = std::make_shared<std::queue<MyClass>>();
+    int num_threads = 10;
+    int iterations = 1000;
+    std::vector<std::thread> mythreads;
+    std::cout << "Initial queue size: " << myqueue->size() << std::endl;
+    for (int i = 0; i < num_threads; ++i) {
+        mythreads.push_back(std::thread(Worker::Task, myqueue, i, iterations));
+    }
+    // wait for all threads to finish
+    for (auto &t : mythreads) t.join();
+    std::cout << "Final queue size: " << myqueue->size() << std::endl;
+    int error_code = 0;
+    if (myqueue->size() != 0) {
+        std::cerr << "Failed: queue size is expected to be zero. Actual "
+                  << myqueue->size() << "." << std::endl;
+        error_code = 1;
+    } else {
+        std::cout << "Success: queue size is zero after all operations."
+                  << std::endl;
+    }
+    // No need to delete myqueue, managed by shared_ptr
+    return error_code;
+}
---
+++ src/tsqueue.h
@@ -0,0 +1,57 @@
+#ifndef TSQUEUE_H
+#define TSQUEUE_H
+
+#include <vector>
+#include <mutex>
+
+template<typename value_type>
+class TSQueue {
+    std::vector<value_type> data;
+    std::mutex mtx;
+
+public:
+    TSQueue() = default;
+    ~TSQueue() = default;
+
+    // Functions are not defined as const even for non-modifying ones,
+    // as mutex variable is modifed by them to get lock (thread safety)
+
+    value_type& back() {
+        std::lock_guard<std::mutex> lock(mtx);
+        return data.back();
+    }
+    template<class... Args>
+    void emplace(Args&&... args) {
+        std::lock_guard<std::mutex> lock(mtx);
+        data.emplace_back(value_type(std::forward<Args>(args)...));
+    }
+
+    bool empty() {
+        std::lock_guard<std::mutex> lock(mtx);
+        return data.empty();
+    }
+
+    value_type& front() {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (data.empty()) std::throw std::runtime_error("TSQueue is empty!")
+        return data.front();
+    }
+
+    void pop() {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (data.empty()) std::throw std::runtime_error("TSQueue is empty!");
+        data.erase(data.begin());
+    }
+
+    void push(value_type& value) {
+        std::lock_guard<std::mutex> lock(mtx);
+        data.push_back(value);
+    }
+
+    size_t size() {
+        std::lock_guard<std::mutex> lock(mtx);
+        return data.size();
+    }
+};
+
+#endif // TSQUEUE_H
